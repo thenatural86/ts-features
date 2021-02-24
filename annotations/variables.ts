bc21472312014e3c6b5514ec7c44e272f435e499
thenatural86 <philip.smalls86@gmail.com>
2021-02-24 16:14:09 -0500

    object types

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -10,3 +10,23 @@ let noithing: undefined = undefined
 
 // built in objects
 let now: Date = new Date()
+
+// Array
+
+let color: string[] = ['red', 'green', 'blue']
+
+let myNumbers: number[] = [1, 2, 3, 4]
+
+let truths: boolean[] = [true, true, false]
+
+// Classes
+
+class Car {}
+
+let car: Car = new Car()
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+}

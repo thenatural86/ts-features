62ced3d4ed5cbcc4270dc8948a767a86c027e670
thenatural86 <philip.smalls86@gmail.com>
2021-02-24 16:05:53 -0500

    variable annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,1 +1,12 @@
-const apples: number = 5
+let apples: number = 5
+
+let speed: string = 'fast'
+
+let hasName: boolean = true
+
+let nothingMuch: null = null
+
+let noithing: undefined = undefined
+
+// built in objects
+let now: Date = new Date()

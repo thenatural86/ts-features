ec2c649d6791b5b3310e05b893a39669544ca9b5
thenatural86 <philip.smalls86@gmail.com>
2021-02-25 15:22:00 -0500

    using interface for annotations

--- interfaces.ts
+++ interfaces.ts
@@ -1,17 +1,19 @@
+interface Vehicle {
+  name: string
+  year: number
+  broken: boolean
+}
+
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true,
 }
 
-const printVehicle = (vehicle: {
-  name: string
-  year: number
-  broken: boolean
-}): void => {
-  console.log(`Name:${vehicle.name}`)
-  console.log(`Year:${vehicle.year}`)
-  console.log(`Broken:${vehicle.broken}`)
+const printVehicle = ({ name, year, broken }: Vehicle): void => {
+  console.log(`Name:${name}`)
+  console.log(`Year:${year}`)
+  console.log(`Broken:${broken}`)
 }
 
 printVehicle(oldCivic)

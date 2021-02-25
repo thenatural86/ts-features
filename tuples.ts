c2454f27b4b836b4c170b8d7c6312a65130f2c1b
thenatural86 <philip.smalls86@gmail.com>
2021-02-25 11:51:16 -0500

    tuples vs objects

--- tuples.ts
+++ tuples.ts
@@ -4,8 +4,17 @@ const drink = {
   sugar: 40,
 }
 
+// type alias
 type Drink = [string, boolean, number]
 
 const pepsi: Drink = ['brown', true, 40]
 const sprite: Drink = ['clear', true, 40]
 const tea: Drink = ['brown', false, 0]
+
+// tuple vs object
+const carSpecs: [number, number] = [400, 3354]
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+}

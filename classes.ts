c12df68b66a70a90ca58e1c78113e2cc39530042
thenatural86 <philip.smalls86@gmail.com>
2021-02-25 16:36:30 -0500

    constructor, super, fileds

--- classes.ts
+++ classes.ts
@@ -9,16 +9,19 @@ class Vehicle {
 const vehicle = new Vehicle('orange')
 console.log(vehicle.color)
 
-// class Car extends Vehicle {
-//   private drive(): void {
-//     console.log('vroom vroom')
-//   }
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color)
+  }
+  private drive(): void {
+    console.log('vroom vroom')
+  }
 
-//   startDrivingProcess(): void {
-//     this.drive()
-//     this.honk()
-//   }
-// }
+  startDrivingProcess(): void {
+    this.drive()
+    this.honk()
+  }
+}
 
-// const car = new Car()
-// car.startDrivingProcess()
+const car = new Car(4, 'red')
+car.startDrivingProcess()

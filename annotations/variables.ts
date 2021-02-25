8a5f043cdff8d32fb717def45a0875e570b3c74b
thenatural86 <philip.smalls86@gmail.com>
2021-02-24 19:58:03 -0500

    second case for using annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -41,3 +41,13 @@ const logNumber: (i: number) => void = (i: number) => {
 const json = '{"x": 10, "y":20}'
 const coordinates: { x: number; y: number } = JSON.parse(json)
 console.log(coordinates) //{x:10, y:20}
+
+// 2)when we declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue']
+let foundWord: boolean
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true
+  }
+}

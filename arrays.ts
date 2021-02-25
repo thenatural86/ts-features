b202233b49d4e22e73894f7e91ad06f753855ed6
thenatural86 <philip.smalls86@gmail.com>
2021-02-25 11:19:35 -0500

    why typed arrays

--- arrays.ts
+++ arrays.ts
@@ -3,3 +3,15 @@ const carMakers = ['ford', 'toyota', 'chevy']
 const dates = [new Date(), new Date()]
 
 const carsByMake = [['f1050'], ['corolla'], ['camaro']]
+
+// help with inference when extracting values
+const car = carMakers[0]
+const myCar = carMakers.pop()
+
+// prevent incompatible values
+carMakers.push() //.push(100)
+
+// help with map
+carMakers.map((car: string): string => {
+  return car
+})
